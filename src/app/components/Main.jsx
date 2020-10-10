572710624da17a13a2daeb985e764088854788c1
Salinas <AXS0JCD@fpl.com>
2020-10-10 14:28:38 -0400

    Initial frontend commit

---
+++ src/app/components/Main.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../store/index';
+import { ConnectedDashboard } from './Dashboard';
+import { Router, Route } from 'react-router-dom';
+import { history } from '../store/history';
+import { ConnectedNavigation } from './Navigation';
+import { ConnectedTaskDetail } from './TaskDetail';
+
+export const Main = () => {
+    return (
+        <Router history={history}>
+            <Provider store={store}>
+                <div>
+                    <ConnectedNavigation />
+                    {/* <ConnectedDashboard /> */}
+                    <Route 
+                        exact path="/dashboard" 
+                        render={() => (<ConnectedDashboard />)} 
+                    />
+                    <Route 
+                        exact path="/task/:id" 
+                        render={({match}) => (<ConnectedTaskDetail match={match} />)} 
+                    />
+                </div>
+            </Provider>
+        </Router>
+    );
+}
---
+++ src/app/components/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div>
+            <Link to="/dashboard">
+                <h1>
+                    My Application
+                </h1>
+            </Link>
+        </div>
+    );
+};
+
+export const ConnectedNavigation = connect(state=>state)(Navigation);
